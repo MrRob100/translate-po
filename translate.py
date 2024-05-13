8298495906bb40daca7e3b06be510fdb777d0d4a
RobA <rob.arnold@ubiquitypress.com>
2024-05-13 16:03:42 -0300

    logging made repetative and test locales removed

--- translate.py
+++ translate.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import os
 import polib
@@ -10,55 +11,81 @@ def perform_translation(text, target_language):
     return translated.text
 
 
-def translate_po_file(source_po_path, target_po_path, target_language):
-    source_po = polib.pofile(source_po_path)
+def translate_po_file(source_po_path, target_language, detail_string = ''):
+    os.path.dirname(source_po_path)
+    directory_path, filename = os.path.split(source_po_path)
+    directory_path_no_locale = directory_path.rsplit('/', 1)[0]
+    target_po_path =  directory_path_no_locale + "/" + target_language + "/" + filename
+    if is_valid_language(target_language):
+        source_po = polib.pofile(source_po_path)
+        target_directory = os.path.dirname(target_po_path)
+        if not os.path.exists(target_directory):
+            os.makedirs(target_directory)
 
-    target_directory = os.path.dirname(target_po_path)
-    if not os.path.exists(target_directory):
-        os.makedirs(target_directory)
+        target_po = polib.POFile()
+        target_po.metadata = {
+            'Language': target_language,
+        }
 
-    target_po = polib.POFile()
+        total_entries = len(source_po)
+        for index, entry in enumerate(source_po):
+            translated_msgstr = perform_translation(entry.msgstr, target_language)
+            new_entry = polib.POEntry(
+                msgid=entry.msgid,
+                msgstr=translated_msgstr
+            )
 
-    target_po.metadata = {
-        'Language': target_language,
-    }
+            target_po.append(new_entry)
+            completion_percentage = (index + 1) / total_entries * 100
+            progress_message = f"{detail_string} {completion_percentage:.2f}% translated into {target_language} "
+#             print(progress_message, end='\r')
+            print(progress_message)
 
-    total_entries = len(source_po)
-    for index, entry in enumerate(source_po):
-        translated_msgstr = perform_translation(entry.msgstr, target_language)
-        new_entry = polib.POEntry(
-            msgid=entry.msgid,
-            msgstr=translated_msgstr
-        )
-        target_po.append(new_entry)
-        completion_percentage = (index + 1) / total_entries * 100
-        progress_message = f"{completion_percentage:.2f}% translated into {target_language}"
-        print(progress_message, end='\r', flush=True)
-
-    target_po.save(target_po_path)
+        target_po.save(target_po_path)
 
 
 def is_valid_language(target_language):
     translator = Translator()
     try:
         translation = translator.translate("test", dest=target_language)
-        if not translation.text:
+        if translation.text:
+            return True
+        else:
             print('translated false')
-            sys.exit(1)
+            return False
     except Exception as e:
         print(e)
-        sys.exit(1)
+        print(target_language)
+        return False
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python translate.py <source_file> <output_lang>")
+    if len(sys.argv) == 2:
+        source_po_path = sys.argv[1]
+        ojs_version = input("OJS version: (eg 3.3 or 3.4. 3.3 uses country explicit locale keys eg 'en_US' whereas 3.4 and above uses just the language eg 'en') ")
+        if ojs_version == '3.3':
+            locale_keys_file = 'locales-3-3.json'
+        elif ojs_version == '3.4' or '3.5':
+            locale_keys_file = 'locales-3-4.json'
+        else:
+            print("Error: Invalid input. Please enter 3.3, 3.4, or 3.5.")
+            sys.exit(1)
+
+
+        with open("keys/" + locale_keys_file) as f:
+            data = json.load(f)
+            total_keys = len(data)
+
+            for index, target_language in enumerate(data, start=1):
+                detail_string = f"{index} out of {total_keys} locales."
+                translate_po_file(source_po_path, target_language, detail_string)
+
+
+    if len(sys.argv) == 3:
+        source_po_path = sys.argv[1]
+        target_language = sys.argv[2]
+        translate_po_file(source_po_path, target_language)
+
+    if len(sys.argv) <2 or len(sys.argv) >3:
+        print("Usage: python translate.py <source_file> <output_lang> leave output_lang blank for all locales")
         sys.exit(1)
-    source_po_path = sys.argv[1]
-    os.path.dirname(source_po_path)
-    directory_path, filename = os.path.split(source_po_path)
-    directory_path_no_locale = directory_path.rsplit('/', 1)[0]
-    target_language = sys.argv[2]
-    target_po_path =  directory_path_no_locale + "/" + target_language + "/" + filename
-    is_valid_language(target_language)
-    translate_po_file(source_po_path, target_po_path, target_language)
